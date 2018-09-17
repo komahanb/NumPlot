98f3f8a9b238bbfb3ef17731d7b336efde34808d
komahan <komahan.cool@gmail.com>
2018-09-17 13:00:39 -0400

    matplotlib customization and tableau colors added

--- numplot/core.py
+++ numplot/core.py
@@ -1,5 +1,45 @@
 from . import helpers
-import matplotlib.pyplot as plt
+
+# Import matplotlib
+import matplotlib.pylab as plt
+
+# Configure 
+plt.rcParams['xtick.direction'] = 'out'
+plt.rcParams['ytick.direction'] = 'out'
+
+# Optionally set font to Computer Modern to avoid common missing font errors
+params = {
+  'axes.labelsize': 20,
+  'legend.fontsize': 14,
+  'xtick.labelsize': 20,
+  'ytick.labelsize': 20,
+  'text.usetex': True}
+plt.rcParams.update(params)
+
+# Latex math
+plt.rcParams['text.latex.preamble'] = [r'\usepackage{sfmath}']
+plt.rcParams['font.family'] = 'sans-serif'
+# plt.rcParams['font.sans-serif'] = 'courier'
+plt.rcParams['font.size'] = 18
+plt.rcParams['font.weight'] = 'bold'
+plt.rcParams['lines.linewidth'] = 4
+plt.rcParams['lines.color'] = 'r'
+
+# Make sure everything is within the frame
+plt.rcParams.update({'figure.autolayout': True})
+
+# These are the "Tableau 20" colors as RGB.    
+tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
+  
+# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
+for i in range(len(tableau20)):    
+    r, g, b = tableau20[i]    
+    tableau20[i] = (r / 255., g / 255., b / 255.)
+
 import numpy as np
 
 def get_hmm():
